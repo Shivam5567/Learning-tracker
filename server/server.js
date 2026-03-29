e629623a948ae9a78db0e4a2cc4c6f7a25f5090e
Shivam5567 <shivam1036kumar@gmail.com>
2026-03-29 15:38:58 +0530

    DB string error sorted

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -32,6 +33,17 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
 });
+
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production' || true) {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+  });
+}
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
